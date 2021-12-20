6f03d9a90db51bf03a1bebacbc234fe16fcbc422
razan <razan3280@gmail.com>
2021-12-20 09:47:41 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,31 @@ const db =require('./db')
 const Todo =require('./todo')
 console.log(Todo);
 
+app.use(express.json())
+
 app.get('/', (req,res)=>{
     res.json('GET / is Working')
 })
 
 app.get('/tasks', (req,res)=>{
-    res.json('GET / is Working')
+    Todo.find({},(err, data)=>{
+        if(err){
+            console.log('ERROR: ', err);
+        }else{
+            res.json(data)
+        }
+    })
+})
+
+app.post('/tasks', (req,res)=>{
+
+    Todo.create(req.body,(err, newTask)=>{
+        if(err){
+            console.log('ERROR: ', err);
+        }else{
+            res.status(201).json(newTask)
+        }
+    })
 })
 
 app.listen(5000, ()=>{
