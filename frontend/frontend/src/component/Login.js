18d50aed119faeb7ac465aec1776aaeae98f88f8
razan <razan3280@gmail.com>
2021-12-28 12:08:38 +0300

    finish bootstrap login

--- frontend/frontend/src/component/Login.js
+++ frontend/frontend/src/component/Login.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 export default function Login(props) {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('razan@gmail.com')
+    const [password, setPassword] = useState('1234')
     
 
     const loginFunc=(e) =>{
@@ -28,7 +28,7 @@ export default function Login(props) {
 
     return (
         <div className='Login'>
-            <form action=''>
+            {/* <form action=''>
                 <label htmlFor=''>Email:</label>
                 <input onChange={(e)=>{
                     setEmail(e.target.value)
@@ -48,8 +48,31 @@ export default function Login(props) {
                 <input type='submit' value='Login' 
                 onClick={loginFunc} className="btn btn-primary"/>
                  <Link to="/register">Don't Have An Account</Link>
-            </form>
-            
+            </form> */}
+            <form>
+            <div class="form-floating mb-3">
+  <input type="email" class="form-control" 
+  id="floatingInput" 
+  onChange={(e)=>{
+    setEmail(e.target.value)
+}} 
+value={email}/>
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="mb-3 form-floating">
+  <input type="password" 
+  class="form-control" 
+  id="floatingPassword"
+  onChange={(e)=>{
+    setPassword(e.target.value)
+}}
+value={password}/>
+  <label for="floatingPassword">Password</label>
+  </div>
+  <input type='submit' value='Login' 
+                onClick={loginFunc} className="btn btn-primary"/>
+                 <Link to="/register" className="btn btn-link">Don't Have An Account</Link>
+                 </form>
         </div>
     )
 }
